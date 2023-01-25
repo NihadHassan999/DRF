4f85b1eac1c595eb386b284f3c664572665e903b
nihad99 <nihad99@gmail.com>
2023-01-25 18:13:07 +0530

    second commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from blog.models import Post, Category
 
-# Create your tests here.
+class Test_Create_Post(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='django')
+        testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789')
+        test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug = 'post-title', author_id = 1, status='published')
+
